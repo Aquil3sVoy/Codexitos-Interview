5cd15d8510c690eb4c3572e3ac8e6dcbe65d1100
afloresmorales <afloresmorales96@gmail.com>
2020-08-13 13:48:56 -0600

    adding instructions for interview.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+/* 
+  1. using this url, fetch some movies from this url: "https://reactnative.dev/movies.json"
+  2. Store the list of movies in a state variable
+  3. Display the list of movies in the browser.
+  4. Add two inputs and a button. (Favorite movie, Release Year, Add Button)
+  5. Add a function that is triggered by the onClick method of the Add Button. 
+  This method must add your favorite movie and release year to the list of movies.
+  6. Sort the movies by ascending order.
+  7. Have the document.title be updated every time the favoriteMovie value changes.
+
+  8. Save your favorite movie using redux.
+  9. Get Favorite movie from redux's store.
+  10. Display the favorite movie gotten from the store.
+*/
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <code>React</code>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
